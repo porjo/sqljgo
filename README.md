bf539c716aff97696aede457580210bfed922f91
Ian Bishop <ianbishop@pace7.com>
2015-01-03 19:56:16 +1000

    Add LICENSE

---
+++ README.md
@@ -0,0 +1,11 @@
+## sqljgo
+
+Convert sqlite3 file to JSON
+
+### Usage
+
+sqljgo is written in Go. Either compile it yourself, or download the precompiled binary from the releases page.
+
+```
+./sqljgo --dbfile data.sqlite > data.json
+```
--- main.go
+++ main.go
@@ -1,3 +1,26 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Ian Bishop
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// sqljgo converts an sqlite input file to json
 package main
 
 import (
@@ -12,14 +35,10 @@ import (
 )
 
 type Tables map[string]Table
-
 type Table []Row
 type Row map[string]interface{}
 
 func main() {
-
-	var tables Tables = make(Tables)
-
 	dbfile := flag.String("dbfile", "", "Sqlite database filename")
 	flag.Parse()
 
@@ -38,6 +57,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	var tables Tables = make(Tables)
 	for rows.Next() {
 		var name string
 		rows.Scan(&name)
@@ -57,6 +77,9 @@ func main() {
 			panic(err.Error())
 		}
 
+		// ------------------------------------------------------------------
+		// Credit to http://play.golang.org/p/jxza3pbqq9 for the following
+		//
 		// Make a slice for the values
 		values := make([]interface{}, len(columns))
 
@@ -67,6 +90,7 @@ func main() {
 		for i := range values {
 			scanArgs[i] = &values[i]
 		}
+		// ------------------------------------------------------------------
 
 		// Fetch rows
 		for rows.Next() {
